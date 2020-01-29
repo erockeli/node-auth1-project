9f3c7100d5985f9fd69382ba73441845ab051f41
Eric Foster <elitheroyalone@gmail.comgit config --global user.name eric-fostergit config --global user.email elitheroyalone@gmail.com>
2020-01-29 15:18:29 -0600

    Add index and server.js and add most of dependenies

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const helmet = require('helmet')
+const session = require('express-session')
+
+const ApiRouter = require('./api/api-router')
+
+const server = express()
+
+server.use(helmet())
+server.use(express.json())
+
+server.use(
+    session({
+        name: 'lovejones',
+        secret: 'keep it secret',
+        cookie: {
+            maxAge: 1 * 24 * 60 * 60 * 1000,
+            secure: false,
+        },
+        httpOnly: true,
+        resave: false,
+        saveUninitialized: false,
+    })
+)
+
+server.use('/api', ApiRouter)
+
+server.use((err, req, res, next) => {
+    console.log(err);
+    res.json({message: "error!!!"})
+})
+
+module.exports = server;
